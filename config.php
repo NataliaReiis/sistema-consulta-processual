1921a8785409f203437a1923ab823841fe21b993
Marcell Dactes <dacmarcell@proton.me>
2022-12-22 13:33:12 -0300

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '250808';
-    $dbName = 'sistemajusdb';
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-?>
